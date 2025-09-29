13f65d740eff2224052d2e17627d6384be488bdf
Sohaib Sajjad <sohaib.sajjad555@gmail.com>
2025-09-29 10:25:48 +0100

    Create index.js
    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+<HTml>
+    <head>
+        <script>
+            alert("Hello");
+        </script>
+    </head>
+    <body>
+        <button onclick="alert('Checking Baggage weight')">Check Baggage</button>
+    </body>
+</HTml>
