06001c568a281c1fdcd03c45e524c3a8a60b0d21
Sohaib Sajjad <sohaib.sajjad555@gmail.com>
2025-09-30 15:43:31 +0100

    bug fixing

--- ext1.js
+++ ext1.js
@@ -1,5 +1,5 @@
 //write a function to alert if overweight (15kg) baggage
-let checkWeight = ()=>{
+function checkWeight = ()=>{
 let weight = parseInt(document.getElementById("weight").value)
 
 if (weight > 16){
@@ -9,7 +9,7 @@ document.getElementById("removed").style.display = "none"
 }
 }
 
-let removeWeight = ()=>{
+function removeWeight = ()=>{
 let weight = parseInt(document.getElementById("weight").value)
 let weightTobeRemoved = parseInt(document.getElementById("removed").value)
 
